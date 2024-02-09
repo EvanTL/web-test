ba692021cbbc9c287146fcd0ef35a30e0e9183c4
EvanTL <evantanakaliman@gmail.com>
2024-02-10 00:45:46 +0800

    Update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,9 +6,11 @@ const Banner = ({title}: {title: string | null}) => {
     <>
         <div className="w-full h-fit relative">
             <img src={banner} alt="kaleka copyright" className="z-0 h-[211px] object-cover w-full"/>
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-                <h1 className="uppercase text-white text-[32px] font-bold">{title}</h1>
-            </div>
+            {title && (
+                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+                    <h1 className="uppercase text-white text-[32px] font-bold">{title}</h1>
+                </div>
+            )}
         </div>
     </>
     )
