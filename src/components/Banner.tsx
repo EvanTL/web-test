7b5ece1d7212d7db7373a99f21a30a89fb17af8a
EvanTL <evantanakaliman@gmail.com>
2024-02-10 18:37:20 +0800

    Add comment

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,6 +6,10 @@ const Banner = ({title}: {title: string | null}) => {
     <>
         <div className="w-full h-fit relative">
             <img src={banner} alt="kaleka copyright" className="z-0 h-[211px] object-cover w-full"/>
+            {/* 
+            Title header to show text on banner, using conditional to avoid rendering when title is null
+            (unnecessary rendering)
+             */}
             {title && (
                 <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
                     <h1 className="uppercase text-white text-[32px] font-bold">{title}</h1>
